3c827af11f9734d831fc15945280b37945d0d537
Daniel Ouilikon <danielouilikon@Daniels-MacBook-Pro.local>
2023-11-22 01:44:12 -0500

    render and heroku setup, readding lab

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import Hello from "./hello.js";
-//import Lab5 from './Lab5.js';
+import Lab5 from './Lab5.js';
 import CourseRoutes from "./courses/routes.js";
 import cors from "cors"; 
 import "dotenv/config";
 const app = express();
 app.use(cors());
 CourseRoutes(app);
-//Lab5(app);
+Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
